1d7a035c6a75f6fc3ee10c5a8a2cd1c93c187e22
karthik-taranath <karthiktaranath@gmail.com>
2024-12-05 16:11:00 -0500

    updated descriptions

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -7,8 +7,8 @@ export const EXPERIENCE = [
     link: 'https://arxiv.org/abs/2409.14262',
     bullets: [
       'Constructed 3D point cloud maps from LiDAR, GPS, IMU, and camera sensor data using SLAM algorithms',
-      'Filtered point clouds of 100M+ points with RANSAC plane fitting to remove ground points and isolate obstacles',
-      'Annotated processed maps according to traversability, creating a labeled and trainable dataset for autonomous navigation',
+      'Reduced point cloud sizes by 95% by using RANSAC plane fitting to remove ground points and isolate obstacles',
+      'Labeled a large-scale dataset for autonomous navigation by annotating processed maps according to traversability',
       'Published the first large-scale, long-range, cross-campus global navigation dataset with multi-modal perception data and multi-category traversability maps (2025 IEEE ICRA)',
     ],
   },
@@ -20,7 +20,7 @@ export const EXPERIENCE = [
     bullets: [
       'Statically analyzed over 30 executables using Ghidra to understand the structure of x64 PE and COFF files',
       'Designed a reverse engineering tool to extract code from encrypted binaries by reading instructions at runtime',
-      'Reduced execution times by 90% by skipping non-branching instructions, circumventing evasive malware tactics',
+      'Optimized execution times by 90% by skipping non-branching instructions, circumventing evasive malware tactics',
     ],
   },
   {
@@ -32,9 +32,9 @@ export const EXPERIENCE = [
     link: 'https://www.umiacs.umd.edu/',
     bullets: [
       'Designed a 3D convolutional neural network classification model to detect COVID-19 from volumetric CT scans',
-      'Implemented a flexible architecture using ragged tensors to account for variable depth in CT scans',
+      'Implemented a flexible architecture using ragged tensors to account for varying depths in each batch',
       'Developed a data pipeline using data loaders to efficiently process a 70GB dataset with limited memory constraints',
-      'Optimized accuracy through hyperparameter tuning to achieve an F1 score of 0.8235',
+      'Refined model accuracy through hyperparameter adjustments, achieving an F1 score of 0.8235',
     ],
   },
   {
@@ -45,8 +45,8 @@ export const EXPERIENCE = [
     location: 'College Park, MD',
     link: 'https://gamera.umd.edu/',
     bullets: [
-      'Modeled a thrust stand to collect statistics about the helicopter’s flight by utilizing Arduino sensors',
-      'Gained insight into the propellers’ performance by determining their drag and lift from a quadratic regression curve',
+      'Programmed Arduino firmware to enable real-time data collection and processing for propeller analysis',
+      'Gained insight into the propellers’ performance by determining their drag and lift with a quadratic regression curve',
     ],
   },
   {
